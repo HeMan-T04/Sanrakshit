c70d500947460c0b1c0bc3cec9c6bdbc813f2572
hemant kathuria <100681483+HeMan-T04@users.noreply.github.com>
2024-05-07 21:35:09 +0530

    updates

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ import threading
 from zapv2 import ZAPv2
 import os
 import requests
+import json
 
 API_KEY = os.getenv("API_KEY")
-
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 zap_proxy_url = 'http://localhost:8080'
-zap = ZAPv2(apikey=API_KEY, proxies={
-            'http': zap_proxy_url, 'https': zap_proxy_url})
+zap = ZAPv2(apikey=API_KEY, proxies={'http': zap_proxy_url, 'https': zap_proxy_url})
 
 ajax_spider_status = None
 spider_completed = False
@@ -27,9 +26,7 @@ def perform_ajax_spider_scan(target_url):
 
     while zap.ajaxSpider.status == "running":
         ajax_spider_status = f"Spider progress: {zap.ajaxSpider.status}"
-        socketio.emit('update_status', {
-                      'status': ajax_spider_status}, namespace='/')
-
+        socketio.emit('update_status', {'status': ajax_spider_status}, namespace='/')
         time.sleep(5)
 
     ajax_spider_results = zap.ajaxSpider.results(target_url)
@@ -38,24 +35,26 @@ def perform_ajax_spider_scan(target_url):
 
     ajax_spider_status = "Spider completed"
     spider_completed = True
-    socketio.emit('update_status', {
-                  'status': ajax_spider_status, 'completed': spider_completed}, namespace='/')
+    socketio.emit('update_status', {'status': ajax_spider_status, 'completed': spider_completed}, namespace='/')
 
     report_path = generate_report(target_url)
+    return report_path
 
 
 def generate_report(target_url):
-    report_title = target_url.replace('://', '_').replace('/', '_')
-    report_path = f"reports/{report_title}_report.json"
-    
+    report_title = "report"
+    report_path = f"reports/{report_title}.json"
+
     # Generate the report in traditional JSON format
     report_content = zap.core.jsonreport(apikey=API_KEY)
-    
+
     # Write the report content to a file
     with open(report_path, "w") as report_file:
         report_file.write(report_content)
-    
+
     return report_path
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
@@ -68,7 +67,16 @@ def report():
 
 @app.route('/show_report')
 def show_report():
-    return render_template('result.html')
+    # Read the JSON report file
+    with open("reports/report.json", "r") as report_file:
+        report_data = json.load(report_file)
+
+    # Extract data from the report
+    total_vulnerabilities = 0
+    for site in report_data['site']:
+        total_vulnerabilities += len(site.get('alerts', []))
+
+    return render_template('result.html', total_vulnerabilities=total_vulnerabilities)
 
 
 @app.route('/download_report/<path:report_path>')
@@ -81,13 +89,11 @@ def download_report(report_path):
 def start_spider(message):
     target_url = message['target_url']
 
-    spider_thread = threading.Thread(
-        target=perform_ajax_spider_scan, args=(target_url,))
+    spider_thread = threading.Thread(target=perform_ajax_spider_scan, args=(target_url,))
     spider_thread.start()
 
     ajax_spider_status = "Spider started"
-    socketio.emit('update_status', {
-                  'status': ajax_spider_status}, namespace='/')
+    socketio.emit('update_status', {'status': ajax_spider_status}, namespace='/')
 
 
 if __name__ == '__main__':
